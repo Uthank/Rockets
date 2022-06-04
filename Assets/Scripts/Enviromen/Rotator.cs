f18c8fdace97bf05a65dde8242f27781b8afca1f
Artyom <otokonori@ya.ru>
2022-06-04 07:58:25 +0300

    Prepare to release commit

--- Assets/Scripts/Enviromen/Rotator.cs
+++ Assets/Scripts/Enviromen/Rotator.cs
@@ -4,6 +4,12 @@ public class Rotator : MonoBehaviour
 {
     [SerializeField] private float _rotateSpeed = 30;
 
+
+    private void Awake()
+    {
+        transform.rotation = Quaternion.Euler(0, Random.Range(0,360), 0);
+    }
+
     private void Update()
     {
         transform.Rotate(Vector3.up, _rotateSpeed * Time.deltaTime);
